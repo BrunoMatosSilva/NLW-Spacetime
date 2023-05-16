6fff5b84d6d19fdfc5750891e3174deb242b7916
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2023-05-15 23:19:36 -0300

    first setup

---
+++ web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-900 text-zinc-50">
+      <h1 className="text-4xl font-bold p-6">Sua cápsula do tempo</h1>
+    </div>
+  )
+}
+
