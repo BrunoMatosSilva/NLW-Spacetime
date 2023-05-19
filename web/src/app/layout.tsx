83b2a8b482f92ba88890b15b1f77e26f7e804b02
BrunoMatosSilva <bmsdevfullstack@gmail.com>
2023-05-18 22:57:33 -0300

    Aula 04

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,11 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/SignIn'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 
@@ -20,12 +25,26 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid grid-cols-2 min-h-screen">
+          <div className="relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translatey-1/2 translate-x-1/2 bg-purple-700 opacity-50 rounded-full blur-full" />
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -15,7 +15,11 @@ export const Profile = () => {
 
           <p className='max-w-[140px] text-sm leading-snug'>
             {name}
-            <a href='' className='block text-red-400 hover:text-red-300 transition-colors'>Quero Sair</a>
+            <a
+            href="/api/auth/logout" 
+            className='block text-red-400 hover:text-red-300 transition-colors'>
+              Quero Sair
+            </a>
           </p>
       </div>
   )
